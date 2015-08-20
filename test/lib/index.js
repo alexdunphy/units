37e0fd952f2e4db66a27946c5dec932064ad294d
Alex Dunphy <alexanderdunphy@gmail.com>
2015-08-20 22:04:22 +0100

    Add sample property to parse tests

--- test/lib/index.js
+++ test/lib/index.js
@@ -13,7 +13,7 @@ describe('units', function() {
     expect(units).to.be.an('object');
   });
 
-  var parseValid = function(value, unit, valueNumber) {
+  var parseValid = function(value, unit, property, valueNumber) {
     expect(units.parse(value + unit)).to.deep.equal({
       'value': typeof valueNumber === 'number'
         ? valueNumber
@@ -24,29 +24,29 @@ describe('units', function() {
 
   it('should parse valid length values', function() {
     for (var i = 0; i < lengthUnits.length; i++) {
-      parseValid(0, lengthUnits[i]);
-      parseValid(1, lengthUnits[i]);
-      parseValid(-1, lengthUnits[i]);
-      parseValid(MAX_SAFE_INTEGER, lengthUnits[i]);
-      parseValid(-MAX_SAFE_INTEGER, lengthUnits[i]);
-      parseValid(0.000001, lengthUnits[i]);
-      parseValid('.000001', lengthUnits[i], 0.000001);
-      parseValid(-0.000001, lengthUnits[i]);
-      parseValid('-.000001', lengthUnits[i], -0.000001);
+      parseValid(0, lengthUnits[i], 'width');
+      parseValid(1, lengthUnits[i], 'width');
+      parseValid(-1, lengthUnits[i], 'width');
+      parseValid(MAX_SAFE_INTEGER, lengthUnits[i], 'width');
+      parseValid(-MAX_SAFE_INTEGER, lengthUnits[i], 'width');
+      parseValid(0.000001, lengthUnits[i], 'width');
+      parseValid('.000001', lengthUnits[i], 'width', 0.000001);
+      parseValid(-0.000001, lengthUnits[i], 'width');
+      parseValid('-.000001', lengthUnits[i], 'width', -0.000001);
     }
   });
 
   it('should parse valid angle values', function() {
     for (var i = 0; i < angleUnits.length; i++) {
-      parseValid(0, angleUnits[i]);
-      parseValid(1, angleUnits[i]);
-      parseValid(-1, angleUnits[i]);
-      parseValid(MAX_SAFE_INTEGER, angleUnits[i]);
-      parseValid(-MAX_SAFE_INTEGER, angleUnits[i]);
-      parseValid(0.000001, angleUnits[i]);
-      parseValid('.000001', angleUnits[i], 0.000001);
-      parseValid(-0.000001, angleUnits[i]);
-      parseValid('-.000001', angleUnits[i], -0.000001);
+      parseValid(0, angleUnits[i], 'rotateX');
+      parseValid(1, angleUnits[i], 'rotateX');
+      parseValid(-1, angleUnits[i], 'rotateX');
+      parseValid(MAX_SAFE_INTEGER, angleUnits[i], 'rotateX');
+      parseValid(-MAX_SAFE_INTEGER, angleUnits[i], 'rotateX');
+      parseValid(0.000001, angleUnits[i], 'rotateX');
+      parseValid('.000001', angleUnits[i], 'rotateX', 0.000001);
+      parseValid(-0.000001, angleUnits[i], 'rotateX');
+      parseValid('-.000001', angleUnits[i], 'rotateX', -0.000001);
     }
   });
 });
