20f9d2275d07c3e41803e7dba33496087ae57be9
Alex Dunphy <alexanderdunphy@gmail.com>
2015-08-20 13:29:08 +0100

    Add valid angle parsing tests

--- test/lib/index.js
+++ test/lib/index.js
@@ -6,23 +6,47 @@
 
 describe('units', function() {
   var lengthUnits = ['%', 'ch', 'cm', 'em', 'ex', 'in', 'mm', 'pc', 'pt', 'px', 'rem', 'vh', 'vmax', 'vmin', 'vw'];
-  var parseValid = function(value, unit) {
-    expect(units.parse(value + unit)).to.deep.equal({'value': value, 'unit': unit});
-  };
+  var angleUnits = ['deg', 'grad', 'rad', 'turn'];
+  var MAX_SAFE_INTEGER = 9007199254740991;
 
   it('should be defined as an object', function() {
     expect(units).to.be.an('object');
   });
+
+  var parseValid = function(value, unit, valueNumber) {
+    expect(units.parse(value + unit)).to.deep.equal({
+      'value': typeof valueNumber === 'number'
+        ? valueNumber
+        : value,
+      'unit': unit
+    });
+  };
 
   it('should parse valid length values', function() {
     for (var i = 0; i < lengthUnits.length; i++) {
       parseValid(0, lengthUnits[i]);
       parseValid(1, lengthUnits[i]);
       parseValid(-1, lengthUnits[i]);
-      parseValid(9007199254740991, lengthUnits[i]);
-      parseValid(-9007199254740991, lengthUnits[i]);
+      parseValid(MAX_SAFE_INTEGER, lengthUnits[i]);
+      parseValid(-MAX_SAFE_INTEGER, lengthUnits[i]);
       parseValid(0.000001, lengthUnits[i]);
+      parseValid('.000001', lengthUnits[i], 0.000001);
       parseValid(-0.000001, lengthUnits[i]);
+      parseValid('-.000001', lengthUnits[i], -0.000001);
+    }
+  });
+
+  it('should parse valid angle values', function() {
+    for (var i = 0; i < angleUnits.length; i++) {
+      parseValid(0, angleUnits[i]);
+      parseValid(1, angleUnits[i]);
+      parseValid(-1, angleUnits[i]);
+      parseValid(MAX_SAFE_INTEGER, angleUnits[i]);
+      parseValid(-MAX_SAFE_INTEGER, angleUnits[i]);
+      parseValid(0.000001, angleUnits[i]);
+      parseValid('.000001', angleUnits[i], 0.000001);
+      parseValid(-0.000001, angleUnits[i]);
+      parseValid('-.000001', angleUnits[i], -0.000001);
     }
   });
 });
