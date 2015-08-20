b0e0d38a46eab255c60988c1ca15c1054fde352d
Alex Dunphy <alexanderdunphy@gmail.com>
2015-08-20 13:17:30 +0100

    Add valid length unit tests

--- test/lib/index.js
+++ test/lib/index.js
@@ -5,7 +5,24 @@
 'use strict';
 
 describe('units', function() {
+  var lengthUnits = ['%', 'ch', 'cm', 'em', 'ex', 'in', 'mm', 'pc', 'pt', 'px', 'rem', 'vh', 'vmax', 'vmin', 'vw'];
+  var parseValid = function(value, unit) {
+    expect(units.parse(value + unit)).to.deep.equal({'value': value, 'unit': unit});
+  };
+
   it('should be defined as an object', function() {
     expect(units).to.be.an('object');
+  });
+
+  it('should parse valid length values', function() {
+    for (var i = 0; i < lengthUnits.length; i++) {
+      parseValid(0, lengthUnits[i]);
+      parseValid(1, lengthUnits[i]);
+      parseValid(-1, lengthUnits[i]);
+      parseValid(9007199254740991, lengthUnits[i]);
+      parseValid(-9007199254740991, lengthUnits[i]);
+      parseValid(0.000001, lengthUnits[i]);
+      parseValid(-0.000001, lengthUnits[i]);
+    }
   });
 });
