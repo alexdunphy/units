bcb286da46d696bc8ff35d36134793aac835464c
Alex Dunphy <alexanderdunphy@gmail.com>
2015-08-29 11:58:00 +0100

    Update deps

--- dist/units.js
+++ dist/units.js
@@ -3,7 +3,7 @@
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
 	else if(typeof define === 'function' && define.amd)
-		define(factory);
+		define([], factory);
 	else if(typeof exports === 'object')
 		exports["units"] = factory();
 	else
